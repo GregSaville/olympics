12081425f62eabfd7835592cf48d8a3a0d7c6d04
Gregory Saville <64148519+GregSaville@users.noreply.github.com>
2024-07-07 15:46:01 -0500

    Good Morning!
    Let's get going.

---
+++ src/main/kotlin/org/savvy/olympics/OlympicsApplication.kt
@@ -0,0 +1,11 @@
+package org.savvy.olympics
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OlympicsApplication
+
+fun main(args: Array<String>) {
+	runApplication<OlympicsApplication>(*args)
+}
---
+++ src/test/kotlin/org/savvy/olympics/OlympicsApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.savvy.olympics
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OlympicsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
